d270007333b6d37db0c6d865cd5983c703e9a8f9
Dipali Khatri <dipali.khatri@bizzappdev.com>
2016-08-19 17:48:09 +0530

    #T742 solve the flack8

--- mail.py
+++ mail.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#/#############################################################################
+#############################################################################
 #
 #    BizzAppDev
 #    Copyright (C) 2004-TODAY bizzappdev(<http://www.bizzappdev.com>).
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#/#############################################################################
+#############################################################################
 
 import logging
 from openerp.tools.config import config
